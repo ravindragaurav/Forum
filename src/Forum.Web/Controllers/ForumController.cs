1cb2e761f435628e045aaa25e33f99b2fbd22719
Gaurav Ravindra <ravindra_gaurav@yahoo.com>
2019-01-06 07:17:41 +0000

    forum controller first try works

---
+++ src/Forum.Web/Controllers/ForumController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Forum.Web.Controllers
+{
+    public class ForumController : Controller
+    {
+        [Route("forum/getquestions")]
+        [HttpGet]
+        public ActionResult GetQuestions()
+        {
+            var questions = "this is a thing here"; 
+            return Json(questions);
+        }
+    }
+}
